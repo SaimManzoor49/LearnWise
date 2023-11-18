d39ad7a95346d0ea6e2860233320721c9e097aa7
unknown <saummanzoor49@gmail.com>
2023-11-18 23:25:32 +0500

    prisma-setup+schema

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -5,12 +5,12 @@ import * as z from 'zod'
 
 import axios from 'axios'
 import { zodResolver } from '@hookform/resolvers/zod'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 
 
-import { 
+import {
     Form,
     FormControl,
     FormLabel,
@@ -18,60 +18,60 @@ import {
     FormField,
     FormMessage,
     FormItem
- } from '@/components/ui/form'
+} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import toast from 'react-hot-toast'
 
- const formSchema = z.object({
-     title:z.string().min(1,{message:"Title is required."})
- })
+const formSchema = z.object({
+    title: z.string().min(1, { message: "Title is required." })
+})
 
 
 export default function page() {
 
     const router = useRouter();
 
-const form = useForm<z.infer<typeof formSchema>>({
-    resolver:zodResolver(formSchema),
-    defaultValues:{
-        title:''
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: ''
+        }
+    })
+
+
+    const { isSubmitting, isValid } = form.formState
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values)
+        try {
+            const response = await axios.post("/api/course", values);
+            router.push(`/teacher/courses/${response.data.id}`)
+        } catch {
+            toast.error('Something went wrong')
+        }
     }
-})
 
 
-const {isSubmitting,isValid} = form.formState
-
-const onSubmit = async(values:z.infer<typeof formSchema>)=>{
-    console.log(values)
-    try{
-        const response = await axios.post("/api/course",values);
-        router.push(`/teacher/courses/${response.data.id}`)
-    }catch{
-        toast.error('Something went wrong')
-    }
-}
-
-
-  return (
-    <div className='max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6 '>
-        <div className="">
-            <h1 className='text-2xl'>Name your course</h1>
-            <p>What would you like to name your course? Don&apos;t worry, you can change it later.</p>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 mt-8' action="">
-                    <FormField control={form.control} name='title' render={({field})=>(
-                        <FormItem>
-                            <FormLabel>Course title</FormLabel>
-                            <FormControl>
-                                <Input disabled={isSubmitting} placeholder="e.g. 'Advance web development'" {...field}  />
-                            </FormControl>
-                            <FormDescription>
-                                What will you teach in this course
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )} />
+    return (
+        <div className='max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6 '>
+            <div className="">
+                <h1 className='text-2xl'>Name your course</h1>
+                <p>What would you like to name your course? Don&apos;t worry, you can change it later.</p>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 mt-8' >
+                        <FormField control={form.control} name='title' render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Course title</FormLabel>
+                                <FormControl>
+                                    <Input disabled={isSubmitting} placeholder="e.g. 'Advance web development'" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    What will you teach in this course
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )} />
 
                         <div className="flex items-center gap-x-2">
                             <Link href={'/'} >
@@ -79,15 +79,15 @@ const onSubmit = async(values:z.infer<typeof formSchema>)=>{
                                     Cancel
                                 </Button>
                             </Link>
-                            <Button type='submit' disabled={!isValid||isSubmitting}>
-                                    Continue
+                            <Button type='submit' disabled={!isValid || isSubmitting}>
+                                Continue
                             </Button>
                         </div>
 
-                </form>
-            </Form>
-        </div>
+                    </form>
+                </Form>
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }

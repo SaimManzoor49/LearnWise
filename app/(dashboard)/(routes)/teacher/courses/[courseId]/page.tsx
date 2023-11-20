79668a46cebd974eae83f7f1443c738198926a9f
unknown <saummanzoor49@gmail.com>
2023-11-20 23:12:09 +0500

    ImageForm

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { redirect } from 'next/navigation';
 import React from 'react'
 import TitleForm from './_components/TitleForm';
 import DescriptionForm from './_components/DescriptionForm';
+import ImageForm from './_components/ImageForm';
 
 export default async function page({params}:{params:{courseId:string}}) {
 
@@ -60,6 +61,10 @@ export default async function page({params}:{params:{courseId:string}}) {
                 courseId={course.id}
                 />
                 <DescriptionForm 
+                initialData={course}
+                courseId={course.id}
+                />
+                <ImageForm 
                 initialData={course}
                 courseId={course.id}
                 />

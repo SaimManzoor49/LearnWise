9ad194e084825559ddacfb2ba3c25b019dc118d6
unknown <saummanzoor49@gmail.com>
2023-12-01 22:17:48 +0500

    search-page

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,20 @@
+import { db } from '@/lib/db'
 import React from 'react'
+import Categories from './_components/Categories'
 
-export default function page() {
+export default async function page() {
+
+  const categories = await db.category.findMany({
+    orderBy:{
+      name:'asc'
+    }
+  })
+
   return (
-    <div>page</div>
+    <div className='p-6'>
+      <Categories 
+      items={categories}
+      />
+    </div>
   )
 }
